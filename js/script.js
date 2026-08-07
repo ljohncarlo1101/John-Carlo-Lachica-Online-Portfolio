c1bfd06f6d1cefa0b40a463e81b729da915963b2
JohnCarlo <2213473@slu.edu.ph>
2026-08-08 01:44:46 +0800

    Fix Navbar Full Width

--- js/script.js
+++ js/script.js
@@ -10,3 +10,13 @@ window.addEventListener("scroll",()=>{
         header.classList.remove("shrink");
     }
 });
+
+// ===============================
+// Hamburger Button
+// ===============================
+const menuToggle = document.getElementById("menu-toggle");
+const navMenu = document.getElementById("nav-menu");
+
+menuToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+});
